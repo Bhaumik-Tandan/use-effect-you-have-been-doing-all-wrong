6f8c5f5a6b1440426ff2f9096dff6a0734ef74a7
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-02-07 12:53:07 +0530

    [add] basic home page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,26 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import components, { ComponentDefinition } from "./pages"; // Correct import statement
-
-interface PageComponentProps {
-  currentPageIndex: number;
-  setCurrentPageIndex: React.Dispatch<React.SetStateAction<number>>;
-}
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
-  const [currentPageIndex, setCurrentPageIndex] = useState(0);
-  const componentToRender = components[currentPageIndex];
 
-  const goToPreviousPage = () => {
-    setCurrentPageIndex((prevIndex) => Math.max(0, prevIndex - 1));
-  };
-
-  const goToNextPage = () => {
-    setCurrentPageIndex((prevIndex) =>
-      Math.min(components.length - 1, prevIndex + 1)
-    );
-  };
 
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <button onClick={goToPreviousPage} disabled={currentPageIndex === 0}>
-                {"<"} Previous
-              </button>
-            </li>
-            <componentToRender.component />
-            <li>
-              <button onClick={goToNextPage} disabled={currentPageIndex === components.length - 1}>
-                Next {" >"}
-              </button>
-            </li>
-          </ul>
-        </nav>
 
         <Routes>
+        <Route
+        path="/"
+            element={<Home/>}
+          />
           {components.map((comp: ComponentDefinition, index: number) => (
-            <Route key={comp.routeName} path={`/${comp.routeName}`} element={<comp.component />} />
+            <Route
+              key={comp.routeName}
+              path={`/${comp.routeName}`}
+              element={<comp.component />}
+            />
           ))}
         </Routes>
 
